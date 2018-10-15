361e5191edda72ce44c495441b9b9e58a2e32288
Grzegorz Walewski <grzesiu2203@gmail.com>
2018-10-15 22:17:35 +0200

    Added missing files

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('post')
+@include('layouts.errors')
+<form method="POST" action="">
+	{{ csrf_field() }}
+	<input class="form-control" type="text" name="title" id="title" placeholder="Title" required>
+	<textarea class="form-control" name="body" id="body" placeholder="Body" required></textarea>
+	<button class="form-control" type="submit">Opublikuj</button>
+</form>
+@endsection
